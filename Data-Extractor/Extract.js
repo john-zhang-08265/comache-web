d0c7bdd2cac9c83ffb1725c946a490365a547126
John Zhang <john-zhang-08265>
2018-09-24 23:33:59 +1200

    FEAR DRIVEN SOFTWARE DEVELOPMENT

--- Data-Extractor/Extract.js
+++ Data-Extractor/Extract.js
@@ -3,7 +3,8 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root1234"
+  password: "root1234",
+  database: "sensor_db"
 });
 
 con.connect(function(err) {
@@ -11,14 +12,20 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
-var queryString = "";
+var queryString = "SELECT * FROM lightSensor";
 
 con.query(queryString, function(err, result) {
+
+  var data = [];
+
   Object.keys(result).forEach(function(key) {
     var row = result[key];
     var timeValue = {
       x: row.timestamp,
-      y: row.value
+      y: row.brightness
     }
-  })
+    data.push(timeValue)
+  });
+
+  console.log(data);
 });
