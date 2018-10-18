bde00462a2ef17f041fd610049e228b0457ac3d5
John Zhang <john-zhang-08265>
2018-10-18 18:23:54 +1300

    ADDED DB SETUP FILE

---
+++ Microcontroller-Logger/setup_db.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE SmartHome_DEV;
+
+CREATE TABLE Metric(
+	metricID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(20),
+	unit VARCHAR(5)
+);
+
+CREATE TABLE DeviceValue(
+	deviceID INTEGER NOT NULL,
+	measureID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	timestamp TIMESTAMP NOT NULL,
+	value FLOAT NOT NULL,
+	rawValue FLOAT,
+	metricID INTEGER,
+	FOREIGN KEY (metricID) REFERENCES Metric(metricID));
+
+INSERT INTO Metric(name, unit) VALUES ('Temperature', '°C');
+INSERT INTO Metric(name, unit) VALUES ('Humidity', '%');
