78cb5018c40927286747f6f70bd64aad79e80ba8
john-zhang-08265 <john.zhang@eroad.com>
2018-10-20 16:46:34 +1300

    Updated wifiTest.ino file

--- Microcontroller-Logger/wifiTest/wifiTest.ino
+++ Microcontroller-Logger/wifiTest/wifiTest.ino
@@ -2,22 +2,35 @@
 #include <MySQL_Connection.h>
 #include <MySQL_Cursor.h>
 
-IPAddress server_addr(13,238,180,245);   // IP of the MySQL server
+//TO DO:
+//Get time
+//Set up LM35 logic
+//Write to database
+
+IPAddress server_addr;   // IP of the MySQL server
+String endpoint;
 char user[] = "root";                     // MySQL user login username
 char password[] = "root1234";    
 
 WiFiClient client;
 MySQL_Connection conn((Client *)&client);
 
+
 void setup()
 {
-  int deviceID = 1;
-  double val = 0; //READ SERIAL
+
   Serial.begin(115200);
   Serial.println();
 
-  WiFi.begin("SPARK-PRR97E", "QVYJACQX39");
+  //Read temperature data and time stamp (Using a moving average filter)
+  int deviceID = 1;
+  double val = 0; //READ SERIAL
+  double rawVal = 0;
 
+  //Store data in buffer
+
+  //Connect to Wifi
+  WiFi.begin("SPARK-PRR97E", "QVYJACQX39");
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -25,15 +38,43 @@ void setup()
     Serial.print(".");
   }
   Serial.println();
-
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
 
-  Serial.println("DB - Connecting...");
-   while (conn.connect(server_addr, 3306, user, password) != true) {
-     delay(500);
-     Serial.print ( "." );
-   }
+  //Ping RDS endpoint for IP address
+  Wifi.hostByName(endpoint, server_addr);
+
+  //Connect to AWS RDS through IP address
+  if server_addr {
+    Serial.println("DB - Connecting...");
+    while (conn.connect(server_addr, 3306, user, password) != true) {
+      delay(1000);
+      Serial.print ( "." );
+    }
+    Serial.print('CONNECTED TO AWS RDS INSTANCE');
+    //WRITE TO DATABASE
+    String macAddr = Wifi.macAddress();
+
+  } else {
+    //If either wifi or database fails to connect, store in buffer
+  }
+  //If sucess, purge buffer
+  //
+  //Sleep
+  
+
+
+  
+
+  
+
+  
+
+  
+
+  
+
+  
 }
 
 void loop() {
